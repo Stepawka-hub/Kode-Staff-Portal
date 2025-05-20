0a5e5f5c65d88e2304798487326fb142bbe7941d
Stepawka <stepan-lvov-04@mail.ru>
2025-05-20 23:22:55 +0300

    feat: Карточки пользователей

--- src/components/staff-list/user-card/user-card.tsx
+++ src/components/staff-list/user-card/user-card.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import s from "./user-card.module.css";
 import { UserCardProps } from "./types";
+import defaultAvatar from "@images/default-avatar.png";
 
 export const UserCard: FC<UserCardProps> = ({ user }) => {
   const { firstName, lastName, userTag, position, avatarUrl } = user;
@@ -8,14 +9,14 @@ export const UserCard: FC<UserCardProps> = ({ user }) => {
   return (
     <article className={s.card}>
       <div className={s.avatar}>
-        <img src={avatarUrl} alt="Avatar" />
+        <img src={defaultAvatar} alt="Avatar" />
       </div>
-      <div className={s.info}>
-        <div className={s.firstLine}>
-          <p>{`${firstName} ${lastName}`}</p>
-          <span>{userTag}</span>
+      <div className={s.data}>
+        <div className={s.info}>
+          <span className={s.name}>{`${firstName} ${lastName}`}</span>
+          <span className={s.tag}>{userTag}</span>
         </div>
-        <div className={s.secondLine}>{position}</div>
+        <div className={s.position}>{position}</div>
       </div>
     </article>
   );
