60fdceeb5a585c1c29106c98300ac2d8ed1c287a
Stepawka <stepan-lvov-04@mail.ru>
2025-05-24 16:34:13 +0300

    styles: Стилизация радиокнопок

--- src/components/sort-selector/sort-selector.tsx
+++ src/components/sort-selector/sort-selector.tsx
@@ -2,6 +2,7 @@ import { getSortBy, setSortBy } from "@slices/search";
 import { useDispatch, useSelector } from "@store";
 import { TSortBy } from "@types";
 import { ChangeEvent, FC } from "react";
+import s from "./sort-selector.module.css";
 
 export const SortSelector: FC = () => {
   const dispatch = useDispatch();
@@ -14,28 +15,37 @@ export const SortSelector: FC = () => {
 
   return (
     <div role="radiogroup" aria-label="Сортировка">
-      <div>
-        <input
-          id="asc"
-          name="sort"
-          type="radio"
-          value="asc"
-          onChange={handleChange}
-          checked={sortBy === "asc"}
-        ></input>
-        <label htmlFor="asc">По алфавиту</label>
-      </div>
-      <div>
-        <input
-          id="birthday"
-          name="sort"
-          type="radio"
-          value="birthday"
-          onChange={handleChange}
-          checked={sortBy === "birthday"}
-        ></input>
-        <label htmlFor="birthday">По дню рождения</label>
-      </div>
+      <h2 className={s.title}>Сортировка</h2>
+      <fieldset className={s.fieldset}>
+        <div className={s.radio}>
+          <input
+            className={s.input}
+            id="asc"
+            name="sort"
+            type="radio"
+            value="asc"
+            onChange={handleChange}
+            checked={sortBy === "asc"}
+          />
+          <label htmlFor="asc" className={s.label}>
+            По алфавиту
+          </label>
+        </div>
+        <div className={s.radio}>
+          <input
+            className={s.input}
+            id="birthday"
+            name="sort"
+            type="radio"
+            value="birthday"
+            onChange={handleChange}
+            checked={sortBy === "birthday"}
+          />
+          <label htmlFor="birthday" className={s.label}>
+            По дню рождения
+          </label>
+        </div>
+      </fieldset>
     </div>
   );
 };
