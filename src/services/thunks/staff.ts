36d495f3a84f6f1547311159d31e3924e1837d1d
Stepawka <stepan-lvov-04@mail.ru>
2025-05-26 14:50:06 +0300

    feat: Вынос проверки времени кэша в отд. файл

--- src/services/thunks/staff.ts
+++ src/services/thunks/staff.ts
@@ -2,21 +2,21 @@ import { getUsers } from "@api";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { setCache } from "@slices/staff";
 import { RootState } from "@store";
+import { checkCacheTime } from "@utils/helpers/cache";
 import { getErrorMessage } from "@utils/helpers/error";
 import { TDepartments, TUser } from "src/types/types";
 
 const GET_USERS = "staff/get-users";
-const CASHE_TIME = 5 * 60 * 1000;
+export const CACHE_TIME = 5 * 60 * 1000;
 
 export const getUsersAsync = createAsyncThunk<TUser[], TDepartments>(
   GET_USERS,
   async (tab, { dispatch, getState, rejectWithValue }) => {
     const state = getState() as RootState;
     const cache = state.staff.cache;
-    const now = Date.now();
 
     const cachedData = cache[tab];
-    if (cachedData && now - cachedData.timestamp <= CASHE_TIME) {
+    if (cachedData && checkCacheTime(cachedData.timestamp, CACHE_TIME)) {
       return cachedData.data;
     }
 
